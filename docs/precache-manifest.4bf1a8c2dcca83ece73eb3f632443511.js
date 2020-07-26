e6f4a353a834a8a6ace4c0d00aad069578eb968a
Junior Moron <jesusdev29@gmail.com>
2020-07-26 16:51:13 -0400

    docs agregado

---
+++ docs/precache-manifest.4bf1a8c2dcca83ece73eb3f632443511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0437e8c8b820488a01f536d58097bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2167d3f1f2feb295dc62",
+    "url": "/static/css/main.8b375495.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2167d3f1f2feb295dc62",
+    "url": "/static/js/main.65147265.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
